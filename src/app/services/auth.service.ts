115c2eeecc81737303747085e4d02020ea993448
joa0neves <unfit4winter@gmail.com>
2021-02-10 02:45:49 +0000

    feito

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
 	}
 
 	register(email: string, password: string, firstname: string, lastname: string,photoUrl:string) {
-		return this.http.post('http://localhost:4000/user/new', { email, password, firstname, lastname, photoUrl });
+		return this.http.post('http://localhost:4000/user/new', { email, password, firstname, lastname, photoUrl:photoUrl });
 	}
 
 	logout(): void {
